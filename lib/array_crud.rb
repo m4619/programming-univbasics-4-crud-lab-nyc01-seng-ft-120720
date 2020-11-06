c4bb55966a16c187dcd8637ef37f9359dc563137
Miguel  Arreola <marreola4619@gmail.com>
2020-11-06 14:43:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  my_list = ["Jacqumues", "Raf Simons", "Karl Lagerfeld"]
+  my_list = ["wow", "I", "am", "really", "learning"]
   my_list.push("arrays!")
 end
 
